9a9bb3910450230b4313bd9f939507debc2e055b
Marcohau20 <haupech2@gmail.com>
2024-02-03 20:39:02 -0500

    cambios en el back

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs'
+
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/create-cert-key.pem'),
+  cert: fs.readFileSync('./secrets/create-cert.pem'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{httpsOptions});
 
   app.setGlobalPrefix("api/v1");
 
